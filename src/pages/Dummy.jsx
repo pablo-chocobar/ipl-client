4352c676280a168a08c694e46a4b7f3f7a67b67b
pablo-chocobar <iampranavpr@gmail.com>
2024-05-09 13:50:43 +0530

    Update Dummy.jsx
    fixed top level await

--- src/pages/Dummy.jsx
+++ src/pages/Dummy.jsx
@@ -1,10 +1,11 @@
 import getBowlingStats from "@/lib/bowler"
 import getBatterOverall from '@/lib/batter';
 
-const f  = await getBowlingStats("R Ashwin");
-const k = await getBatterOverall("CH Gayle")
+
 
 async function pla() {
+    const f  = await getBowlingStats("R Ashwin");
+    const k = await getBatterOverall("CH Gayle")
 
     console.log(k);
     console.log(f);

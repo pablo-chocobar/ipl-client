e9455969e33dc355b799f5aee2463cd017a15e75
pablo-chocobar <iampranavpr@gmail.com>
2024-05-10 14:20:10 +0530

    switched rank pages to sql

--- src/pages/BatRank.jsx
+++ src/pages/BatRank.jsx
@@ -1,30 +1,63 @@
 import { React, useState, useEffect } from 'react'
 import { batRankColumns } from "../components/Columns"
 import { DataTable } from "../components/BattingRankTable"
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { gte, and } from 'drizzle-orm';
+import { batting } from '@/db/schema';
+import * as schema from "@/db/schema";
+import { Input } from "../components/ui/input"
 
+
+const POSTGRES_URL = "postgres://default:79BDCyZPscYU@ep-falling-sound-a14b3thc-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
+
+const sql = neon(POSTGRES_URL);
+const db = drizzle(sql, {
+    schema
+});
 
 function BatRank() {
 
-    const [inns, setInns] = useState(0);
-    const [runs, setRuns] = useState(0);
-    const [balls, setBalls] = useState(0);
+    const [inns, setInns] = useState("");
+    const [runs, setRuns] = useState("");
+    const [balls, setBalls] = useState("");
+
+    // const [inns, balls, runs] = [0, 0, 0];
 
     const [data, setData] = useState({});
 
+    // async function dataloader() {
+    //     const args = [inns , balls , runs]
+
+    //     const url = `https://ipl-sabermetrics.onrender.com/api/bat?arguments=${encodeURIComponent(JSON.stringify(args))}`;
+
+    //     fetch(url, {
+    //         method: 'POST',
+    //     }).then((response) => response.json()).then(data => {setData(JSON.parse(data))});
+    // }
     async function dataloader() {
-        const args = [inns , balls , runs]
-
-        const url = `https://ipl-sabermetrics.onrender.com/api/bat?arguments=${encodeURIComponent(JSON.stringify(args))}`;
-
-        fetch(url, {
-            method: 'POST',
-        }).then((response) => response.json()).then(data => {setData(JSON.parse(data))});
+        let [b,r,i] = [balls , runs , inns];
+        if(i == "")
+            i = 0;
+        if(b == "")
+            b = 0;
+        if(r == "")
+            r = 0;
+    
+        const batting_stats = await db.select().from(batting).where(
+            and(
+                gte(batting.balls, Number(b)),
+                gte(batting.runs, Number(r)),
+                gte(batting.innings, Number(i))
+            )
+        ).then(
+            response => (setData(response))
+        );
     }
 
     useEffect(() => {
         dataloader();
-    } , []);
-
+    }, [balls , runs , inns]);
 
     return (
 
@@ -33,6 +66,41 @@ function BatRank() {
                 <Navbar></Navbar>
             </div> */}
             <DataTable columns={batRankColumns} data={data} />
+            <div className="flex items-center mx-4 space-x-2 py-4">
+                <div className="flex items-center py-4">
+                    <Input
+                        type = "number"
+                        value={balls?? ""} 
+                        placeholder="Min. balls played"
+                        onChange={(event) =>
+                            setBalls(event.target.value)
+                        }
+                        className="max-w-sm"
+                    />
+                </div>
+                <div className="flex items-center py-4">
+                    <Input
+                        type = "number"
+                        value={runs?? ""} 
+                        placeholder="Min. runs scored"
+                        onChange={(event) =>
+                            setRuns(event.target.value)
+                        }
+                        className="max-w-sm"
+                    />
+                </div>
+                <div className="flex items-center py-4">
+                    <Input
+                        type = "number"
+                        value={inns?? ""}
+                        placeholder="Min. inns played"
+                        onChange={(event) =>
+                            setInns(event.target.value)
+                        }
+                        className="max-w-sm"
+                    />
+                </div>
+            </div>
 
         </>
 
--- src/pages/BowlRank.jsx
+++ src/pages/BowlRank.jsx
@@ -1,38 +1,92 @@
 import { React, useState, useEffect } from 'react'
-import Navbar from "../components/Navbar"
 import { bowlRankColumns } from "../components/Columns"
 import { DataTable } from "../components/BattingRankTable"
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { gte, and } from 'drizzle-orm';
+import {bowling } from '@/db/schema';
+import * as schema from "@/db/schema";
+import { Input } from "../components/ui/input"
 
+
+const POSTGRES_URL = "postgres://default:79BDCyZPscYU@ep-falling-sound-a14b3thc-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
+
+const sql = neon(POSTGRES_URL);
+const db = drizzle(sql, {
+    schema
+});
 
 function BowlRank() {
 
-    const [inns, setInns] = useState(0);
-    const [runs, setRuns] = useState(0);
-    const [balls, setBalls] = useState(0);
+    const [inns, setInns] = useState("");
+    const [wickets, setWickets] = useState("");
+    const [balls, setBalls] = useState("");
 
     const [data, setData] = useState({});
 
     async function dataloader() {
-        const args = [inns, balls, runs]
-        const url = `https://ipl-sabermetrics.onrender.com/api/bowl?arguments=${encodeURIComponent(JSON.stringify(args))}`;
-
-        fetch(url, {
-            method: 'POST',
-        }).then((response) => response.json()).then(data => { setData(JSON.parse(data)) });
+        let [b,w,i] = [balls , wickets , inns];
+        if(i == "")
+            i = 0;
+        if(b == "")
+            b = 0;
+        if(w == "")
+            w = 0;
+    
+        const bowling_stats = await db.select().from(bowling).where(
+            and(
+                gte(bowling.balls, Number(b)),
+                gte(bowling.wickets, Number(w)),
+                gte(bowling.innings, Number(i))
+            )
+        ).then(
+            response => (setData(response))
+        );
     }
 
     useEffect(() => {
         dataloader();
-    }, []);
-
+    }, [balls , wickets , inns]);
 
     return (
-
         <>
             <DataTable columns={bowlRankColumns} data={data} />
-
+            <div className="flex items-center mx-4 space-x-2 py-4">
+                <div className="flex items-center py-4">
+                    <Input
+                        type = "number"
+                        value={balls?? ""} 
+                        placeholder="Min. balls bowled"
+                        onChange={(event) =>
+                            setBalls(event.target.value)
+                        }
+                        className="max-w-sm"
+                    />
+                </div>
+                <div className="flex items-center py-4">
+                    <Input
+                        type = "number"
+                        value={wickets?? ""} 
+                        placeholder="Min. wickets taken"
+                        onChange={(event) =>
+                            setWickets(event.target.value)
+                        }
+                        className="max-w-sm"
+                    />
+                </div>
+                <div className="flex items-center py-4">
+                    <Input
+                        type = "number"
+                        value={inns?? ""}
+                        placeholder="Min. inns played"
+                        onChange={(event) =>
+                            setInns(event.target.value)
+                        }
+                        className="max-w-sm"
+                    />
+                </div>
+            </div>
         </>
-
     )
 }
 

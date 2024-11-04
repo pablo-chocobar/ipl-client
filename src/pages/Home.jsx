f1abee0dba8d9169bae83436501450696a3a247c
pablo-chocobar <iampranavpr@gmail.com>
2024-11-04 23:04:53 +0530

    lol

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,12 +12,12 @@
             <>
                 <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 text-left">
                     <HomeCard link = "/player" fill={true} text="Player profile" subtext="See how a player has performed in the IPL"></HomeCard>
-                    <div className="grid md:grid-cols-3 gap-8">
-                        <HomeCard link = "/headon" text="Head vs Head" subtext="See how a player has performed in the IPL"></HomeCard>
+                    <div className="grid md:grid-cols-2 gap-8">
+                        {/* <HomeCard link = "/headon" text="Head vs Head" subtext="See how a player has performed in the IPL"></HomeCard> */}
                         <HomeCard link = "/batrank" text="Batting Ranks" subtext="Batting Rankings"></HomeCard>
                         <HomeCard link = "/bowlrank" text = "Bowling Ranks" subtext="Bowling Rankings"></HomeCard>
                     </div>
-                    <HomeCard link = "/dummy" text = "dummy" subtext="REMOVE THIS"></HomeCard>
+                    {/* <HomeCard link = "/dummy" text = "dummy" subtext="REMOVE THIS"></HomeCard> */}
 
                 </div>
             </>
